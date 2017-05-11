439be433df143be6bf3ca90fe336ac57e82563df
Big Bad Waffle <skichenbrand@gmail.com>
2017-05-11 19:52:02 +0200

    Fixes #144

--- src/client/js/components/chest.js
+++ src/client/js/components/chest.js
@@ -4,18 +4,28 @@ define([
 
 ) {
 	var colors = [
-		'f2f5f5',
+		'9a5a3c',
 		'3fa7dd',
+		'faac45',
 		'a24eff',
-		'ff6942'
+		'ffeb38'
 	];
 
 	var chances = [
-		0.02,
-		0.05,
+		0.01,
+		0.03,
+		0.055,
 		0.1,
-		0.17
+		0.16
 	];
+
+	var indices = {
+		'50': 0,
+		'51': 1,
+		'128': 2,
+		'52': 3,
+		'53': 4
+	};
 
 	return {
 		type: 'chest',
@@ -35,13 +45,15 @@ define([
 				}
 			}
 
-			var color = colors[this.obj.cell - 50];
+			var index = indices[this.obj.cell];
+
+			var color = colors[index];
 
 			this.obj.addComponent('particles', {
-				chance: chances[this.obj.cell - 50],
+				chance: chances[index],
 				blueprint: {
 					color: {
-						start: colors[this.obj.cell - 50]
+						start: colors[index]
 					},
 					alpha: {
 						start: 0.75,
@@ -51,7 +63,7 @@ define([
 						start: 1,
 						end: 4
 					},
-					chance: chances[this.obj.cell - 50]
+					chance: chances[index]
 				}
 			});
 		},
